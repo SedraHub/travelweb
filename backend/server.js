bf67e26c3617aa9565636adf91a84a4d97e3b9e4
SedraHub <fl.alsham2004@gmail.com>
2026-03-24 03:41:21 +0300

    backend eklendi

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/comments", require("./routes/comments"));
+
+app.listen(5000, () => {
+    console.log("Server: http://localhost:5000");
+});
