1a85214cedc3c8b50cd9215e7abd403eac4499d5
SedraHub <fl.alsham2004@gmail.com>
2026-03-31 04:10:03 +0300

    first pull

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 
+const contactRoute = require("./routes/contact");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/contact", contactRoute);
 
 app.use("/api/comments", require("./routes/comments"));
 
